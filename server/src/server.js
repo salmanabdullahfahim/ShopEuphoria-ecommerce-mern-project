545f898ee38573f69ca0dcc02591802c7b7b0082
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-05 17:41:31 +0600

    some middleware added

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,31 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan());
+//middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded());
 
+const isLoggedIn = (req, res, next) => {
+  const login = true;
+  if (login) {
+    req.body.id = 101;
+    next();
+  } else {
+    return res.status(401).json({ message: "please login first" });
+  }
+};
+
+//api
 app.get("/test", (req, res) => {
   res.status(200).send({
     message: "api is working properly",
+  });
+});
+app.get("/api/user", isLoggedIn, (req, res) => {
+  console.log(req.body.id);
+  res.status(200).send({
+    message: "User is returned successfully",
   });
 });
 
