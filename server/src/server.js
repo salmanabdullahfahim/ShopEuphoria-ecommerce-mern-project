b5210333a3001e8f0f9e0293d285f36a6d39d6b0
Salman Abdullah Fahim <77009176+salmanabdullahfahim@users.noreply.github.com>
2023-10-03 10:53:42 +0600

    Delete server directory for some problem
    I will make the project again from scratch

--- server/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const connectDB = require("./config/db");
-const { serverPort } = require("./secret");
-
-app.listen(serverPort, async () => {
-  console.log(`server is running on port ${serverPort}`);
-
-  await connectDB();
-});
