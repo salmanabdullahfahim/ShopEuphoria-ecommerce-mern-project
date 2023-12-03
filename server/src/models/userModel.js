21cf065bdbf46272a701f33d01f8409a90addedd
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-03 18:38:36 +0600

    again basic express server setup done

--- server/src/models/userModel.js
+++
@@ -1,7 +0,0 @@
-const users = [
-  { id: 1, name: "fahim" },
-  { id: 2, name: "rahim" },
-  { id: 3, name: "karim" },
-];
-
-module.exports = users;
--- server/src/secret.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const serverPort = process.env.PORT || 5002;
-const mongoURL =
-  process.env.MONGODB_URL || "mongodb://localhost:27017/shopEuphoriaDB";
-
-module.exports = { serverPort, mongoURL };
