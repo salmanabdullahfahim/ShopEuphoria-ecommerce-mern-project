5cc5d26e34c87c0391ff9f5269ae90dd6e3cfb1e
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-04 21:25:38 +0600

    added morgan package for HTTP request logging

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/test", (req, res) => {
   res.status(200).send({
