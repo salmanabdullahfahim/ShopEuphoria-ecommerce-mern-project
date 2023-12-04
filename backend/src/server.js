2f11d5d23b9e164947b6c63a7cdf1ff4bad8c117
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-04 18:57:19 +0600

    add a test api for test the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "Api is working",
+  });
+});
+
 app.listen(3001, () => {
   console.log("listening on 3001");
 });
