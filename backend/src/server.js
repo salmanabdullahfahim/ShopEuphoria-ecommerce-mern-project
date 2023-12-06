9daad5cc1c01c12c2d33e9a0b90f04d094e4e6e2
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-06 06:36:43 +0600

    added 2 built in middleware json and urlencoded

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,27 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const isLoggedIn = (req, res, next) => {
+  const login = false;
+  if (login) {
+    next();
+  } else {
+    res.status(401).json({ message: "You must be logged in" });
+  }
+};
 
 app.get("/test", (req, res) => {
   res.status(200).send({
     message: "Api is working",
+  });
+});
+
+app.get("/user", isLoggedIn, (req, res) => {
+  res.status(200).send({
+    message: "User profile is here",
   });
 });
 
