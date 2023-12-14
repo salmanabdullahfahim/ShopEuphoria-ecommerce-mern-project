a41606b03046f1192812017513a5496a7b22bf37
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-14 07:29:14 +0600

    added server error handler

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,6 +31,13 @@ app.get("/user", isLoggedIn, (req, res) => {
 //client error handler
 app.use((req, res, next) => {
   res.status(404).json({ message: "route not found" });
+  next();
+});
+
+//server error handler
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: "something broke" });
 });
 
 app.listen(3001, () => {
