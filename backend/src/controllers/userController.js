b778bd5120b4d4925550523491b5b8d744c33f96
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-22 21:24:43 +0600

    userSchema created

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,11 +1,9 @@
 const createError = require("http-errors");
-const users = require("../models/userModel");
 
 const getUsers = (req, res, next) => {
   try {
     res.status(200).send({
       message: "User profile is here",
-      users: users,
     });
   } catch (error) {
     next(error);
--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,16 +1,53 @@
-const users = [
-  {
-    id: 1,
-    name: "Fahim",
-  },
-  {
-    id: 2,
-    name: "Nayeem",
-  },
-  {
-    id: 3,
-    name: "Rahim",
-  },
-];
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-module.exports = users;
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      minLength: [3, "User name must be at least 3 characters"],
+      maxLength: [26, "User name must be at most 26 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "User email is required"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(v);
+        },
+        message: "Please enter a valid email address",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "User password is required"],
+      minLength: [6, "Password must be at least 6 characters"],
+      set: (v) => bcrypt.hashSync(v, bcrypt.genSaltSync(10)),
+    },
+    image: {
+      type: String,
+    },
+    address: {
+      type: String,
+      required: [true, "User address is required"],
+    },
+    phone: {
+      type: String,
+      required: [true, "User phone is required"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isBanned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
