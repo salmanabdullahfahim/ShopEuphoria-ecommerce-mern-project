79fda66b0e37279745be6f4f346f206ac723a978
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-24 20:20:12 +0600

    searching functionality added for GET Users API

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -3,7 +3,22 @@ const User = require("../models/userModel");
 
 const getUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
+    const search = req.query.search || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+
+    const searchRegex = new RegExp(".*" + search + ".*", "i");
+
+    const filter = {
+      isAdmin: { $ne: true },
+      $or: [
+        { name: { $regex: searchRegex } },
+        { email: { $regex: searchRegex } },
+        { phone: { $regex: searchRegex } },
+      ],
+    };
+
+    const users = await User.find(filter);
     res.status(200).send({
       message: "User profile is here",
       users,
