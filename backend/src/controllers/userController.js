45576dff08838d13708c6bc9474cae027a832847
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-12-24 20:38:52 +0600

    pagination functionality added in the Users api

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,7 +5,7 @@ const getUsers = async (req, res, next) => {
   try {
     const search = req.query.search || "";
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 5;
+    const limit = Number(req.query.limit) || 1;
 
     const searchRegex = new RegExp(".*" + search + ".*", "i");
 
@@ -18,10 +18,25 @@ const getUsers = async (req, res, next) => {
       ],
     };
 
-    const users = await User.find(filter);
+    const options = { password: 0 };
+
+    const users = await User.find(filter, options)
+      .limit(limit)
+      .skip((page - 1) * limit);
+
+    const count = await User.find(filter).countDocuments();
+
+    if (!users) throw createError(404, "No users found");
+
     res.status(200).send({
-      message: "User profile is here",
+      message: "Users are here",
       users,
+      pagination: {
+        totalPage: Math.ceil(count / limit),
+        currentPage: page,
+        previousPage: page - 1 > 0 ? page - 1 : null,
+        nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+      },
     });
   } catch (error) {
     next(error);
